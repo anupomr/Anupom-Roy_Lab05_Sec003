f69a76d9e9942b284a5b19284258c97a921ec981
Anupom Roy <anupomr@yahoo.com>
2018-07-20 14:29:22 -0400

    FullStackException class is created

---
+++ Anupom-Roy_Lab05_Ex02/FullStackException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anupom_Roy_Lab05_Ex02
+{
+    public class FullStackException: Exception
+    {
+        // parameterless constructor
+        public FullStackException() : base("Stack is full")
+        {
+            // empty constructor
+        }
+
+        // one-parameter constructor
+        public FullStackException(string exception) : base(exception)
+        {
+            // empty constructor
+        }
+
+        // two-parameter constructor
+        public FullStackException(string exception, Exception inner)
+           : base(exception, inner)
+        {
+            // empty constructor
+        }
+    }
+}
