fa282f05eeaeab92558e0185ebf701fc836429b9
Anupom Roy <anupomr@yahoo.com>
2018-07-21 10:59:59 -0400

    exception handle for invalid argument

--- Anupom-Roy_Lab05_Ex02.1/GenericMethod.cs
+++ Anupom-Roy_Lab05_Ex02.1/GenericMethod.cs
@@ -19,6 +19,15 @@ namespace Anupom_Roy_Lab05_Ex02._1
             DisplayArray(intArray); // pass an int array argument
             Console.Write("Array intArray index 1 to 4: ");
             DisplayArray(intArray,1,4); // pass an int array argument
+            try
+            {
+                DisplayArray(intArray, -1, 4);
+            }
+            catch (ArgumentException e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+            
             Console.Write("Array doubleArray contains: ");
             DisplayArray(doubleArray); // pass a double array argument
             Console.Write("Array charArray contains: ");
@@ -38,7 +47,17 @@ namespace Anupom_Roy_Lab05_Ex02._1
         // Overload generic method                           
         private static void DisplayArray<T>(T[] inputArray, int lowIndex, int highIndex)
         {
-     
+            if (lowIndex<0 ||highIndex<0)
+            {
+                throw new ArgumentException(); }
+            if (lowIndex > highIndex )
+            {
+                throw new ArgumentException("First Argument suppose to lower then Second argument");
+            }
+            if (highIndex > inputArray.Length)
+            {
+                throw new ArgumentException("Second argument is heigher then array size");
+            }
             T[] newArray=new T[highIndex];           
             int i = 0;            
             while (lowIndex <= highIndex )
