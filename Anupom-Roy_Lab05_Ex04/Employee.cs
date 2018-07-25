29b7a61e016b03b55c92da2d0f6b22e9bbff9c1f
Anupom Roy <anupomr@yahoo.com>
2018-07-25 19:13:40 -0400

    Employee class is created

---
+++ Anupom-Roy_Lab05_Ex04/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anupom_Roy_Lab05_Ex04
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double salary;
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value > 0)
+                    salary = value;
+                else
+                    salary = 2000.0;
+            }
+        } // end Salary property
+        public Employee(string name, double empSalary)
+        {
+            Name = name;
+            Salary = empSalary;
+        }
+        public override string ToString() => $"Employee: {Name}  Salary{Salary}";
+    }
+}
---
+++ Anupom-Roy_Lab05_Ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anupom_Roy_Lab05_Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
